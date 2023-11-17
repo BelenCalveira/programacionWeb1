d662856b9a4e7c128b4a448683e304db4aa11f5b
Belen <lohreley@hotmail.com>
2023-11-17 18:32:30 -0300

    listo para revisar

--- codigo.js
+++ codigo.js
@@ -1,18 +1,3 @@
-/*var nota = 7
-console.log("He realizado mi examen")
-if (nota < 5)
-  calificacion = "Insuficiente"
-else if (nota < 6)
-  calificación = "Suficiente"
-else if (nota < 8)
-  calificacion = "Bien"
-else if (nota <= 9)
-  calificacion = "Notable"
-else
-  calificacion = "Sobresaliente"
-console.log("He obtenido un", calificacion)
-var otro = document.getElementById("basico").is
-console.log(otro)*/
 function validarComplejidad() {
     if (document.getElementById('b').checked) {
         document.getElementById('basico').style.display = 'block';
@@ -30,4 +15,107 @@ function validarComplejidad() {
         document.getElementById('avanzado').style.display = 'block';
     }
 
+}
+let punto = 0;
+
+function puntaje() {
+    if (document.getElementById('b').checked) {
+        punto = 0;
+        if (document.getElementById('b1a').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b2b').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b3a').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b4c').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b5af').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b5bv').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b5cv').checked) {
+            punto = punto + 1;
+
+        }
+        if (document.getElementById('b5dv').checked) {
+            punto = punto + 1;
+
+        }
+    } else if (document.getElementById('i').checked) {
+        punto = 0;
+        if (document.getElementById('i1b').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i2b').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i3b').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i4c').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i5af').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i5bv').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i5cv').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('i5dv').checked) {
+            punto = punto + 1;
+        }
+    } else if (document.getElementById('a').checked) {
+        punto = 0;
+        if (document.getElementById('a1c').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a2b').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a3a').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a4c').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a5af').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a5bv').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a5cv').checked) {
+            punto = punto + 1;
+        }
+        if (document.getElementById('a5dv').checked) {
+            punto = punto + 1;
+        }
+    }
+
+}
+
+function enviar() {
+    var nombre = document.getElementById('nomb').value;
+    puntaje();
+    let tblDato = document.getElementById('tablaDeP').insertRow(5);
+    let col1 = tblDato.insertCell(0);
+    let col2 = tblDato.insertCell(1);
+
+    col1.innerHTML = nombre;
+    col2.innerHTML = punto;
 }
